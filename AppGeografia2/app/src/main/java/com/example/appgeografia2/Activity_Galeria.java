35d51c35da2303814d5296abefb506d64453c45a
Voidfractur <73500723+Voidfractur@users.noreply.github.com>
2021-01-28 08:29:58 -0600

    Update Activity_Galeria.java

--- AppGeografia2/app/src/main/java/com/example/appgeografia2/Activity_Galeria.java
+++ AppGeografia2/app/src/main/java/com/example/appgeografia2/Activity_Galeria.java
@@ -4,21 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.Gallery;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class Activity_Galeria extends AppCompatActivity {
-
+    ImageView imagenSeleccionada;
+    Gallery gallery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__galeria);
-        String urlGif = "https://i.pinimg.com/originals/c2/28/48/c2284898b3a5d832d4d16084628bca49.gif";
-        //Agregar implementacion Glide dentro de archivo build.gradle.
-        ImageView im1 = (ImageView)findViewById(R.id.im1);
-        Uri uri = Uri.parse(urlGif);
-        Glide.with(getApplicationContext()).load(uri).into(im1);
+      
     }
 }
